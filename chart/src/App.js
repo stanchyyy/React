c901585a2df0468297218eeea7364310fbf0e1a5
stanchyyy <stanchyyy@gmail.com>
2022-02-01 23:09:15 +0200

    added proof of concept card

---
+++ chart/src/App.js
@@ -0,0 +1,13 @@
+import Header from "./Components/Header";
+import BarCharts from "./Components/BarCharts";
+
+function App() {
+  return (
+    <div>
+      <Header title = 'Charts'/>
+      <BarCharts/>
+    </div>
+  );
+}
+
+export default App;
