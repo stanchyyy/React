9c0d1dd6f805f2fd711c4955f75fa6428e313794
stanchyyy <stanchyyy@gmail.com>
2022-02-03 02:09:52 +0200

    added 4 images for each skill, added for each card

--- cards/src/App.js
+++ cards/src/App.js
@@ -1,12 +1,46 @@
 import ActionPane from "./Components/ActionPane";
-import Card from "./Components/Card";
+let  pointsToDistribute = Math.floor(Math.random() * 20 + 1);
+let defaultPoints = 1;
+let maxPoints = 10;
+let baseSkillPoints = 5;
+let maxBaseSkillPoints = 25;
+
+const skills = [{
+  id : 1,
+  imageSource : '/images/Illustration-de-Nutchapol-Thitinunthakorn_STRENGHT_.jpg',
+  defaultPoints :  baseSkillPoints,
+  maxPoints : maxBaseSkillPoints,
+  attributeName : 'STRENGHT',
+  },
+  {
+  id : 2,
+  imageSource : '/images/Illustration-de-Slawomir-Maniak_AGILITY_.jpg',
+  defaultPoints : defaultPoints,
+  maxPoints : maxPoints,
+  attributeName : 'AGILITY',
+  },
+  {
+  id : 3,
+  imageSource : '/images/Illustration-de-Milivoj-Ceran_INTELIGENCE_.jpg',
+  defaultPoints : defaultPoints,
+  maxPoints : maxPoints,
+  attributeName : 'INTELIGENCE',
+  },
+  {
+  id : 4,
+  imageSource : 'images/Illustration-des-mondes-de-blizzard_VITALITY_.jpg',
+  defaultPoints : defaultPoints,
+  maxPoints : maxPoints,
+  attributeName : 'VITALITY',
+  },
+];
+
 
 
 function App() {
   return (
     <div>
-      <ActionPane pointsToDistribute = {Math.floor(Math.random() * 20 + 1)}/>
-      <Card/>
+      <ActionPane pointsToDistribute = {pointsToDistribute} skills = {skills}/>
     </div>
   );
 }
--- cards/src/Components/ActionPane.js
+++ cards/src/Components/ActionPane.js
@@ -1,9 +1,24 @@
-import styles from './ActionPane.module.css'; 
+import styles from './ActionPane.module.css';
+
 const ActionPane = (props) =>{
+
     return (
-<div className={styles.ActionPane}>
-You have <span className={styles.ActionPanePoints}>{props.pointsToDistribute}</span> skill points to distribute.
-</div>
+        <div>
+            <div className={styles.ActionPane}>
+                You have <span className={styles.ActionPanePoints}>{props.pointsToDistribute}</span> skill points to distribute.
+            </div>
+            <div className={styles.row}>
+                    {props.skills.map((skill)=>(
+                        <div className={styles.column} key ={skill.id} style={{backgroundImage : 'url('+skill.imageSource+')'}}>
+                        <div className={styles.card}>
+                        <h3>{skill.attributeName}</h3>
+                        <p>{skill.defaultPoints}</p>
+                        </div>
+                        </div>
+
+                    ))}
+            </div>
+        </div>
     )
 }
 
